425dd67b30ff5a8cd5093a97ae6aac0c23b13998
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2025-07-29 16:51:23 -0300

    .

--- script.js
+++ script.js
@@ -19,5 +19,18 @@ lista.textContent = linguagens[0]; // altera o elemento HTML da variável "lista
 //lista.textContent = linguagens[1]; //
 //lista.textContent = linguagens[2];
 
+let aluno1 = {
+  nome: "Joanir",
+  idade: 43,
+  anoLetivo: "Universidade",
+  materiasFavoritas: ["Filosofia","Programação","Arte"]
+}
+
+console.log(aluno1.nome); // “Gabriel”
+console.log(aluno1.idade); // 13
+console.log(aluno1.materiasFavoritas);
+// ["Geografia","Programação","Arte"]
+console.log(aluno1.materiasFavoritas[1]);
+
 const objeto = document.querySelector("#aluno");
 objeto.textContent = aluno1.nome;
