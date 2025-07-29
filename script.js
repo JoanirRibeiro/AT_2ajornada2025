6f78fc45825137bb401bac5122d44758130d3ff7
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2025-07-29 16:24:55 -0300

    .

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ console.log(linguagens[0]) // “JavaScript”
 console.log(linguagens[1]) // “Python”
 console.log(linguagens[2]) // “C++”
 
-
 // Adicionar valores da lista na página com DOM.
 const lista = document.querySelector("#lista");  //seleciona elemento com id "lista" e guarda na variável "lista"
+lista.textContent = linguagens[1];
 lista.textContent = linguagens[0]; // altera o elemento HTML da variável "lista" para o valor 0 da lista "linguagens"
 lista.textContent = linguagens[1]; 
-lista.textContent = linguagens[2]; 
+lista.textContent = linguagens[2];
